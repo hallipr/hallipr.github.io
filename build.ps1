c8f4e7f800a601a2225553044d54642fc2aaef06
Patrick Hallisey <hallipr@gmail.com>
2024-03-22 21:10:08 -0700

    Next app

--- build.ps1
+++ build.ps1
@@ -9,20 +9,21 @@ try {
     $ENV:PUBLIC_URL = "/new-app"
     LogCommand { npm run build -ws --if-present }
 
-    if (Test-Path "./build") {
-        Remove-Item -Recurse -Force build
+    $outFolder = "./out"
+    if (Test-Path $outFolder) {
+        Remove-Item -Recurse -Force $outFolder
     }
 
-    New-Item -ItemType Directory -Path "./build" | Out-Null
+    $outFolder = New-Item -ItemType Directory -Path $outFolder
 
     Write-Host "Copy react site to build/new-app folder"
-    Copy-Item -Path "./new-app/build" -Destination "./build/new-app" -Recurse -Force | Out-Null
+    Copy-Item -Path "./new-app/out" -Destination "$outFolder/new-app" -Recurse -Force | Out-Null
 
     Write-Host "Copy static site to build/breeding folder"
-    Copy-Item -Path "./breeding" -Destination "./build/breeding" -Recurse -Force | Out-Null
+    Copy-Item -Path "./breeding" -Destination "$outFolder/breeding" -Recurse -Force | Out-Null
 
     Write-Host "Copy static index to build folder"
-    Copy-Item -Path "./index.html" -Destination "./build" -Recurse -Force | Out-Null
+    Copy-Item -Path "./index.html" -Destination $outFolder | Out-Null
 }
 finally {
     Pop-Location
--- new-app/next.config.mjs
+++ new-app/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/new-app',
+};
 
 export default nextConfig;
