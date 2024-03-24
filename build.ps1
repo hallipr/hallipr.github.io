3aee4cc56a980cd30e6eba99014282a0d3d96a2f
Patrick Hallisey <hallipr@gmail.com>
2024-03-24 01:58:48 +0000

    Make new-app the whole static site

--- build.ps1
+++ build.ps1
@@ -6,7 +6,6 @@ function LogCommand($command) {
 Push-Location -Path $PSScriptRoot
 try {
     LogCommand { npm ci }
-    $ENV:PUBLIC_URL = "/new-app"
     LogCommand { npm run build -ws --if-present }
 
     $outFolder = "./out"
@@ -17,13 +16,7 @@ try {
     $outFolder = New-Item -ItemType Directory -Path $outFolder
 
     Write-Host "Copy react site to build/new-app folder"
-    Copy-Item -Path "./new-app/out" -Destination "$outFolder/new-app" -Recurse -Force | Out-Null
-
-    Write-Host "Copy static site to build/breeding folder"
-    Copy-Item -Path "./breeding" -Destination "$outFolder/breeding" -Recurse -Force | Out-Null
-
-    Write-Host "Copy static index to build folder"
-    Copy-Item -Path "./index.html" -Destination $outFolder | Out-Null
+    Copy-Item -Path "./new-app/out" -Destination "$outFolder" -Recurse -Force | Out-Null
 }
 finally {
     Pop-Location
--- new-app/next.config.mjs
+++ new-app/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/new-app',
+    basePath: process.env.basePath,
+    env: {
+      basePath: process.env.basePath,
+    },
 };
 
 export default nextConfig;
